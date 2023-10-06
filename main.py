2d839d92c10948f83e7b4a7dfdf0ad83c6066e89
minicoders <yossifaziz29@gmail.com>
2023-10-06 20:54:31 +0300

    added a trash code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5 import QtWidgets, uic
 from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog, QScrollBar, QComboBox, QColorDialog, QCheckBox, QSlider
 from PyQt5.QtWidgets import *
-
+from PyQt5.QtCore import QTimer
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
@@ -23,6 +23,8 @@ from fpdf import FPDF
 from wfdb.io.record import rdrecord
 import wfdb
 
+import modules
+
 class MainWindow(QtWidgets.QMainWindow):
 
         # Mainwindow constructor
@@ -33,17 +35,53 @@ class MainWindow(QtWidgets.QMainWindow):
           self.setWindowTitle("Realtime-signal-viewer")
           self.xAxis = [0,0,0]
           self.yAxis = [0,0,0]
-
-
-
+          self.pointsToAppend = 0
+          self.PlotterWindowProp = modules.PlotterWindow()
+          self.PauseToggleVar = False
+          self.HoldVarH = False
+          self.HoldVarV = False
+          self.SignalChannelArr = []
+          for i in range(3):
+               self.SignalChannelArr.append(modules.SignalChannel())
+          4
+          # params
+          self.pushButton_2.clicked.connect(self.browse)
+          # 
+          self.channel1Signal = pg.PlotCurveItem()
+          self.channel1TimeReadings = [0,0]
+          self.channel1AmplitudeReadings = [0,0]
+          self.channel1PlottedXCoordinates = []
+          self.channel1PlottedYCoordinates = []
+          self.isChannel1Open, self.isChannel2Open, self.isChannel3Open = False, False, False
+          self.isChannel1Shown, self.isChannel2Shown, self.isChannel3Shown = False, False, False
+          
 
       # browse function to open directory : a+m
-      
+      def browse(self):
+            self.fileName = QFileDialog.getOpenFileName(None,"Open a File","./",filter="Raw Data(*.txt *.csv *.xls *.hea *.dat)" )
+            if self.fileName[0]:
+                 self.openFile(self.fileName[0])   
 
       # open the file from directory : a+m
+      def openFile(self, path:str):
+            timeArr, amplitudeArr = [],[]
+            length = len(path)
+            fileExtentsion = path[length-3:]
+            if fileExtentsion == "csv" or fileExtentsion == "txt" or fileExtentsion == "xls":
+               with open(path, 'r') as file:
+                csv_data = csv.reader(file, delimiter=',')
+                for row in csv_data:
+                      timeArr.append(float(row[0]))
+                      amplitudeArr.append(float(row[1]))
+
+            self.SignalChannelArr[modules.choosenChannel].time =  timeArr
+            self.SignalChannelArr[modules.choosenChannel].amplitude = amplitudeArr
+            self.plotSignal()
 
 
       # initialize plotting: a+m
+
+
 
 
       # draw plot: a+m
