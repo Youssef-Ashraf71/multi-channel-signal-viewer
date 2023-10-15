0c0d5e2d75420db69a03d91832b28133e71e0b65
minicoders <yossifaziz29@gmail.com>
2023-10-15 15:59:32 +0300

    Revert "addmod1"
    This reverts commit 0cc0bff0fd76c98f9a2f6b428f0acb3f22a0c569.

--- main.py
+++ main.py
@@ -67,7 +67,6 @@ class MainWindow(QtWidgets.QMainWindow):
           self.rewindLinkBtn.hide()
           self.isSyncingX = False
           self.about_clicked = False 
-          self.numImg = 1
           connector.__init__connectors__(self)
           
 
@@ -884,8 +883,8 @@ class MainWindow(QtWidgets.QMainWindow):
           exporter.params.fileSuffix = 'png'
 
           # Set the filename
-          export_filename = "img%s.png"%(self.numImg)
-          self.numImg += 1
+          export_filename = 'graph_capture.png'
+
           # Export the graph to the specified filename
           exporter.export(export_filename)
 
@@ -1102,17 +1101,15 @@ class MainWindow(QtWidgets.QMainWindow):
           pdf.drawImage(college_logo, 525, 705, width=70, height=70)
           
           # Open the image file
-          image = Image.open('img1.png')
+          image = Image.open('graph_capture.png')
           # Get the size (width and height) of the image
           image_width, image_height = image.size
           aspect_ratio = image_width / image_height
           # Close the image file
           image.close()
-          for i in range(self.numImg-1): 
-             #  print(self.numImg,"aaaaaaaaa")    
-               plotImg = ImageReader('img%s.png'%(i+1))
-               pdf.drawImage(plotImg, 35, 500+20*i, width=550, height=400 / aspect_ratio)
-               
+          
+          plotImg = ImageReader('graph_capture.png')
+          pdf.drawImage(plotImg, 35, 500, width=550, height=400 / aspect_ratio)
           statistics_table = self.calculatePlotStatistics(graphNumber)
           statistics_table.wrapOn(pdf, 0, 0)
           statistics_table.drawOn(pdf, 60, 200)  
